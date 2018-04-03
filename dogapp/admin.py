3ed8a12e0e546ce12ed2831bc49664822e6170f1
Tomas <anavarro.lestonnac@gmail.com>
2018-04-03 12:15:07 +0200

    Models created (have to fix little things). Admins can add Refuges, Dogs and Vaccines to the DB.

---
+++ dogapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from dogapp import models
+
+admin.site.register(models.Dog)
+admin.site.register(models.Vaccine)
+admin.site.register(models.Refuge)
+
+# Register your models here.
---
+++ dogapp/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Refuge(models.Model):
+    name = models.TextField()
+    street = models.TextField(blank=True, null=True)
+    number = models.IntegerField(blank=True, null=True)
+    city = models.TextField(blank=True, null=True)
+    zipCode = models.TextField(blank=True, null=True)
+    stateOrProvince = models.TextField(blank=True, null=True)
+    country = models.TextField(blank=True, null=True)
+    telephone = models.TextField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    user = models.ForeignKey(User, default=1)
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+
+class Vaccine(models.Model):
+    name = models.TextField()
+    description = models.TextField(blank=True, null=True)
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+
+class Dog(models.Model):
+    name = models.TextField()
+    race = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField('Euro amount', max_digits=8, decimal_places=2, blank=True, null=True)
+    # image = models.ImageField(upload_to="myrestaurants", blank=True, null=True)
+    refuge = models.ForeignKey(Refuge, null=True, related_name='dogs')
+    vaccine = models.ForeignKey(Vaccine, null=True, related_name='dogs')
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+
+class Client(models.Model):
+    name = models.TextField()
+    description = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, default=1)
+    password = models.TextField(blank=True, null=True)
+    gender = models.TextField(blank=True, null=True)
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+
+class Adoption(models.Model):
+    name = models.TextField()
+    user = models.ForeignKey(Client, null=True, related_name='adoption')
+    dog = models.ForeignKey(Dog, null=True, related_name='adoption')
