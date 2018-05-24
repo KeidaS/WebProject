41b3277a590fd2d79f27e4c9de715389bcafd19d
KeidaS <anavarro.lestonnac@gmail.com>
2018-05-24 13:07:34 +0200

    Added json

--- features/register_refuge.feature
+++ features/register_refuge.feature
@@ -1,12 +1,12 @@
 Feature: Register Restaurant
   In order to keep track of the existent refuges
-  As a admin
-  I want to register a refuge with its location and contact details
+  As a user
+  I want to register a refuge
 
   Background: There is a registered user
     Given Exists an user "user" with password "password"
 
-  Scenario: I don't register a refuge as user
+  Scenario: Can't create a refuge as a registered user.
     Given I login as user "user" with password "password"
     Then There is no "dogapp:refuge_create" link available
     And There are 0 refuges
--- features/steps/authentication.py
+++ features/steps/authentication.py
@@ -3,33 +3,33 @@ from behave import *
 use_step_matcher("parse")
 
 
-@given('Exists a user "{username}" with password "{password}"')
+@given('Exists an user "{username}" with password "{password}"')
 def step_impl(context, username, password):
     from django.contrib.auth.models import User
     User.objects.create_user(username=username, email='user@example.com', password=password)
 
-
+'''
 @given('Exists a admin "{username}" with password "{password}"')
 def step_impl(context, username, password):
     from django.contrib.auth.models import User
     User.objects.create_user(username=username, email='user@example.com', password=password, admin=True)
-
+'''
 
 @given('I login as user "{username}" with password "{password}"')
 def step_impl(context, username, password):
-    context.browser.visit(context.get_url('/accounts/login/?next=/myrestaurants/'))
+    context.browser.visit(context.get_url('/accounts/login/'))
     form = context.browser.find_by_tag('form').first
     context.browser.fill('username', username)
     context.browser.fill('password', password)
-    form.find_by_css('button.btn-success').first.click()
+    form.find_by_value('login').first.click()
     assert context.browser.is_text_present(username)
 
-
+'''
 @given('I\'m not logged in')
 def step_impl(context):
     context.browser.visit(context.get_url('logout') + '?next=/myrestaurants/')
     assert context.browser.is_text_present('Login')
-
+'''
 
 @then('Server responds with page containing "{message}"')
 def step_impl(context, message):
